02813c7fd579d383f1fd7ed6488b3fb1cb87fbd4
sana2211 <70413361+sana2211@users.noreply.github.com>
2021-01-13 00:14:04 +0400

    Create test.js
    test

---
+++ test.js
@@ -0,0 +1,136 @@
+'use strict';
+
+const app = require('../app');
+const expect = require('chai').expect;
+const request = require('supertest');
+const data = require('../data.js');
+
+
+describe('playstore GET /app endpoint', () => {
+
+  it('return all the apps', () => {
+    return request(app)
+      .get('/app') // invoke the endpoint // send the query string ?n=5
+      .then(res => {
+        expect(res.body).to.eql(store);
+      });
+  });
+
+  
+    it('invalid sort', () => {
+    return request(app)
+      .get('/app') // invoke the endpoint //assert that you get a 200  OK status
+      .query({'sort':'name',})
+      .expect(400, 'Can only sort by rating or app');
+  });
+
+  
+  
+  it('invalid sort valid genre', () => {
+    return request(app)
+      .get('/app') // invoke the endpoint //assert that you get a 200  OK status
+      .query({'sort':'name',
+    'genres':'Action'})
+      .expect(400, 'Can only sort by rating or app');
+  });
+
+
+    
+  it('invalid genre', () => {
+    return request(app)
+      .get('/app') // invoke the endpoint //assert that you get a 200  OK status
+      .query({'genres':'notgenre',})
+      .expect(400, 'can only filter by Action,Puzzle,Strategy,Casual,Arcade,or Card');
+  });
+
+
+      
+  it('valid sort invalid genre', () => {
+    return request(app)
+      .get('/app') // invoke the endpoint //assert that you get a 200  OK status
+      .query({'genres':'notgenre',
+                'sort': 'app'})
+      .expect(400, 'can only filter by Action,Puzzle,Strategy,Casual,Arcade,or Card');
+  });
+
+  it('Valid sort', () => {
+    return request(app)
+      .get('/app') // invoke the endpoint //assert that you get a 200  OK status
+      .query({'sort':'app',})
+      .then(res => {
+        expect(res.body).to.be.an('array');
+        let i = 0;
+        let sorted = true;
+        while(sorted && i < res.body.length - 1) {
+          sorted = sorted && res.body[i].App < res.body[i + 1].App;
+          i++;
+        }
+        expect(sorted).to.be.true;
+      });
+  });
+
+  it('Valid sort', () => {
+    return request(app)
+      .get('/app') // invoke the endpoint //assert that you get a 200  OK status
+      .query({'sort':'rating',})
+      .then(res => {
+        expect(res.body).to.be.an('array');
+        let i = 0;
+        let sorted = true;
+        while(sorted && i+1 < res.body.length - 1) {
+          sorted = sorted && res.body[i].Rating >= res.body[i + 1].Rating;
+          i++;
+        }
+        expect(sorted).to.be.true;
+      });
+  });
+
+  it('filter by genre', () => {
+    return request(app)
+      .get('/app') // invoke the endpoint //assert that you get a 200  OK status
+      .query({'genres':'Action',})
+      .then(res => {
+        expect(res.body).to.be.an('array');
+        expect(res.body).to.have.lengthOf(6);
+      });
+  });
+  
+  it('sort by app name and filter by action ', () => {
+    return request(app)
+      .get('/app') // invoke the endpoint //assert that you get a 200  OK status
+      .query({'genres':'Action',
+            'sort':'app'})
+      .then(res => {
+        expect(res.body).to.be.an('array');
+        expect(res.body).to.have.lengthOf(6);
+        let i = 0;
+        let sorted = true;
+        while(sorted && i < res.body.length - 1) {
+          sorted = sorted && res.body[i].App < res.body[i + 1].App;
+          i++;
+        }
+        expect(sorted).to.be.true;
+
+      });
+  });
+
+
+
+  /*  it('should error out', () => {
+    return request(app)
+      .get('/frequency') // invoke the endpoint //assert that you get a 200  OK status
+      .expect(400, 'Invalid request');
+  });
+  it('should give an object back with details about string zzzz', () => {
+    return request(app)
+      .get('/frequency') // invoke the endpoint
+      .query({'s':'zzzz',}) // send the query string ?n=5
+      .expect(200)  //assert that you get a 200  OK status
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).to.deep.include({'z':4,'unique':1,'average':4,'highest':'z'});
+      });
+  });
+  */
+
+});
